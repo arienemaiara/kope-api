5cc8164386ef3b0774a6f89215757ec2316b7051
Ariene Maiara <ariene.maiara@hotmail.com>
2020-04-18 15:59:31 -0300

    Inclusão .env

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'docker',
-    database: 'kope',
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     define: {
         timestamps: true,
         underscored: true,
