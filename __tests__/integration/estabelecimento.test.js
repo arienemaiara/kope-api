e16f8cf65c78dc64a42c7d7024a6083678ccc121
Ariene Maiara <ariene.maiara@hotmail.com>
2020-04-25 01:52:47 -0300

    Inclusão casos de teste

---
+++ __tests__/integration/estabelecimento.test.js
@@ -0,0 +1,40 @@
+import request from 'supertest';
+
+import factory from '../factories';
+import app from '../../src/app';
+
+describe('Estabelecimento', () => {
+
+    it('Deverá ser cadastrado com sucesso', async () => {
+
+    });
+
+    it('Não poderá cadastrar CNPJ inválido', async () => {
+
+    });
+
+    it('Não poderá cadastrar CPF inválido', async () => {
+
+    });
+
+    it('Não poderá cadastrar CPF/CNPJ duplicado', async () => {
+
+    });
+
+    it('Não poderá cadastrar e-mail duplicado', async () => {
+
+    });
+
+    it('Não poderá alterar CPF/CNPJ', async () => {
+
+    });
+
+    it('Não deixar passar senha anterior inválida', async () => {
+
+    });
+
+    it('Deverá ser alterado com sucesso', async () => {
+
+    });
+
+});
---
+++ __tests__/integration/movimentacoes.test.js
@@ -0,0 +1,18 @@
+import request from 'supertest';
+
+import factory from '../factories';
+import app from '../../src/app';
+
+describe('Movimentacoes', () => {
+    it('Deverá acumular pontos', async () => {
+
+    });
+
+    it('Deverá resgatar pontos', async () => {
+
+    });
+
+    it('Não permite o resgate sem pontos suficientes', async () => {
+
+    });
+});
