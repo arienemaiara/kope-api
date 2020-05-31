cb7badc0ca819d03c55967cda53dee58f67d5d57
Ariene Maiara <ariene.maiara@hotmail.com>
2020-05-30 23:06:36 -0300

    Variavel database heroku

--- src/database/index.js
+++ src/database/index.js
@@ -16,7 +16,7 @@ class Database {
     }
 
     init() {
-        this.connection = new Sequelize(databaseConfig);
+        this.connection = new Sequelize(process.env.DATABASE_URL || databaseConfig); //Heroku ou local
         
         models
             .map((model) => model.init(this.connection))
