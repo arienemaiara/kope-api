094be52d05ca187de4eec5764b351b18ce50efca
Ariene Maiara <ariene.maiara@hotmail.com>
2020-05-31 00:54:06 -0300

    Configuração bd testes

--- src/config/database.js
+++ src/config/database.js
@@ -14,6 +14,18 @@ module.exports = {
             underscoredAll: true,
         }
     },
+    test: {
+        dialect: process.env.DATABASE_DIALECT || 'sqlite',
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        storage: './__tests__/database.sqlite',
+        define: {
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true,
+        }
+    },
     production: {
         use_env_variable: 'DATABASE_URL',
         define: {
